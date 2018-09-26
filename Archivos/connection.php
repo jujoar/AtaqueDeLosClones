aa76c0487dd2bad6ad6d36bef7c3364338319a79
Sanchez7 <luisktkd@hotmail.com>
2018-09-25 22:16:13 -0600

    Phishing attack

---
+++ Archivos/connection.php
@@ -0,0 +1,5 @@
+<?php
+	$db = mysqli_connect('localhost', 'id7266590_clon', 'root123', 'id7266590_clon');
+	//Usuario, contraseña y nombre de la BD.
+	mysqli_set_charset($db,'utf8'); //Para que muestre bien los datos que tienen acento o ñ
+?> 
